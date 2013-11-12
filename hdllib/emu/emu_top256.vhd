3b03c3434939c8bca5ce5ff385b1d307fd2da565
Artem Shvorin <art@shvorin.net>
2013-11-12 21:06:22 +0400

    эмулятор переводится на интерфейс ast

--- hdllib/emu/emu_top256.vhd
+++ hdllib/emu/emu_top256.vhd
@@ -22,17 +22,19 @@ architecture emu_top256 of emu_top256 is
 
     signal clk, reset : std_logic;
 
-    -- rx
-    signal rx_data                  : tlp256.data_t;
-    signal rx_dvalid                : std_logic;
-    signal rx_sop, rx_eop, ej_ready : std_logic;
-    --
-    -- tx
-    signal tx_data                  : tlp256.data_t;
-    signal tx_dvalid                : std_logic;
+    signal ast_rx, ast_tx : ast;
+    signal ast_tx_bp      : ast_bp;
 
     -- data representation for foreing calls
     type foreign_tlp256_data_t is array (integer range 0 to 7) of integer;
+
+    type foreign_ast is record
+        data  : foreign_tlp256_data_t;
+        valid : std_logic;
+        sop   : std_logic;
+        eop   : std_logic;
+        empty : std_logic_vector(1 downto 0);
+    end record;
 
     function wrap(data : tlp256.data_t) return foreign_tlp256_data_t is
         variable result : foreign_tlp256_data_t;
@@ -54,11 +56,20 @@ architecture emu_top256 of emu_top256 is
         return result;
     end;
 
+    function wrap(a : ast) return foreign_ast is
+    begin
+        return (wrap(a.data), a.valid, a.sop, a.eop, a.empty);
+    end;
+
+    function unwrap(f : foreign_ast) return ast is
+    begin
+        return (unwrap(f.data), f.valid, f.sop, f.eop, f.empty);
+    end;
+
     -- About linking with foreign functions see
     -- http://ghdl.free.fr/ghdl/Restrictions-on-foreign-declarations.html
 
-    procedure line256_up(tx_dvalid       : std_logic;
-                         foreign_tx_data : foreign_tlp256_data_t)
+    procedure line256_up(foreign_ast_tx : foreign_ast)
     is
     begin
         assert false severity failure;
@@ -66,23 +77,14 @@ architecture emu_top256 of emu_top256 is
 
     attribute foreign of line256_up : procedure is "VHPIDIRECT line256_up";
 
-    -- NB: corresponding C prototype is: void line256_down(struct scalar_params *, uint32_t arr[8])
-    procedure line256_down(
-        -- scalar parameters
-        rx_dvalid       : out std_logic;
-        rx_sop, rx_eop  : out std_logic;
-        ej_ready        : out std_logic;
-        -- composite parameter(s)
-        foreign_rx_data : out foreign_tlp256_data_t)
+    procedure line256_down(foreign_ast_rx : out foreign_ast;
+                           ast_tx_bp      : out ast_bp)
     is
     begin
         assert false severity failure;
     end;
 
     attribute foreign of line256_down : procedure is "VHPIDIRECT line256_down";
-
-    signal ast_rx, ast_tx : ast;
-    signal ast_tx_bp      : ast_bp;
 
 begin
     cg : entity work.clock_gen
@@ -98,37 +100,25 @@ begin
             tx    => ast_tx,
             tx_bp => ast_tx_bp);
 
-    ast_rx          <= (rx_data, rx_dvalid, rx_sop, rx_eop, (others => '0'));
-    tx_data         <= ast_tx.data;
-    tx_dvalid       <= ast_tx.valid;
-    ast_tx_bp.ready <= ej_ready;
-
-
     data_down : process (clk, reset)
-        variable v_rx_dvalid, v_rx_sop, v_rx_eop, v_ej_ready : std_logic;
-        variable foreign_rx_data                             : foreign_tlp256_data_t;
+        variable foreign_ast_rx : foreign_ast;
+        variable v_ast_tx_bp    : ast_bp;
     begin
         if reset = '1' then
-            rx_data   <= (others => '0');
-            rx_dvalid <= '0';
-            rx_sop    <= '0';
-            rx_eop    <= '0';
-            ej_ready  <= '0';
-
+            ast_rx    <= nothing;
+            ast_tx_bp <= (ready => '0');
         elsif rising_edge(clk) then
-            line256_down(v_rx_dvalid, v_rx_sop, v_rx_eop, v_ej_ready, foreign_rx_data);
-            rx_data   <= unwrap(foreign_rx_data);
-            rx_dvalid <= v_rx_dvalid;
-            rx_sop    <= v_rx_sop;
-            rx_eop    <= v_rx_eop;
-            ej_ready  <= v_ej_ready;
+            line256_down(foreign_ast_rx, v_ast_tx_bp);
+
+            ast_rx    <= unwrap(foreign_ast_rx);
+            ast_tx_bp <= v_ast_tx_bp;
         end if;
     end process;
 
     data_up : process (clk)
     begin
         if rising_edge(clk) then
-            line256_up(tx_dvalid, wrap(tx_data));
+            line256_up(wrap(ast_tx));
         end if;
     end process;
     
