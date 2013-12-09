4813541e0eda32dfdfce5aa128369f05e1dbbaac
Artem Shvorin <art@shvorin.net>
2013-12-09 13:18:11 +0400

    c6f7d7b1cf0 (skifch разделен на skifch_inj и skifch_ej) — для эмулятора

--- examples/loopback/emu_conf.vhd
+++ examples/loopback/emu_conf.vhd
@@ -1,13 +1,52 @@
 -- Copyright (c) 2011-2014, Ailamazyan Program Systems Institute (Russian             
 -- Academy of Science). See COPYING in top-level directory.
 
-use work.tlp_package.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use work.ast256.all;
+
+entity pautina_ast_loopback is
+    port (
+        clk   : in std_logic;
+        reset : in std_logic;
+
+        -- Avalon-ST
+        ast_rx       : in  ast_t;
+        ast_tx       : out ast_t;
+        ast_tx_bp    : in  ast_bp_t;
+        rx_st_bardec : in  std_logic_vector(7 downto 0));
+end entity;
+
+architecture pautina_ast_loopback of pautina_ast_loopback is
+    use work.vdata.all;
+
+    signal int_vdata : vflit256_t;
+    signal int_ready : std_logic;
+begin
+    p_ast : entity work.pautina_ast
+        port map (
+            clk   => clk,
+            reset => reset,
+
+            tx_vdata => int_vdata,
+            tx_ready => int_ready,
+
+            rx_vdata => int_vdata,
+            rx_ready => int_ready,
+
+            ast_rx       => ast_rx,
+            ast_tx       => ast_tx,
+            ast_tx_bp    => ast_tx_bp,
+            rx_st_bardec => rx_st_bardec);
+end architecture;
+
+
 use work.ast256.all;
 
 configuration emu_conf of emu_top256 is
     for emu_top256
         for app : ast_ext_io
-            use entity work.pautina_ast(pautina_ast_impl)
+            use entity work.pautina_ast_loopback
                 port map (
                     clk          => clk,
                     reset        => reset,
