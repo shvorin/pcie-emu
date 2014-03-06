14adbd14ca5c7a018b5192d4eee562982881197a
Artem Shvorin <art@shvorin.net>
2014-03-06 18:36:46 +0400

    из модулей выброшены generic параметры SKIFCH_QTY и LINK_QTY, вместо них заведены константы

--- examples/loopback/emu_conf.vhd
+++ examples/loopback/emu_conf.vhd
@@ -5,7 +5,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use work.ast256.all;
 use work.avmm.all;
-use work.cc_meta.all;                   -- FIXME
+use work.pautina_package.all;
 
 entity pautina_ast_loopback is
     port (
