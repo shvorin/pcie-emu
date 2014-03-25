574be728e9d6e6fc63bfd4a550703a5ce534ea7f
Artem Shvorin <art@shvorin.net>
2014-03-26 00:37:06 +0400

    параметры skifch_num и qsfp_num переименованы и перенесены в configure.vhd

--- examples/loopback/emu_conf.vhd
+++ examples/loopback/emu_conf.vhd
@@ -22,8 +22,6 @@ end entity;
 architecture pautina_ast_loopback of pautina_ast_loopback is
     use work.vdata.all;
 
-    subtype skifch_range is integer range 0 to SKIFCH_QTY-1;
-
     signal int_vdata : vdata256_array(skifch_range);
     signal int_ready : std_logic_vector(skifch_range);
 begin
