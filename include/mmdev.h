088f7de67c18b78f6b9627451ccd7fc9d9eacd57
Yuri Klimov <yuri@klimov.net>
2014-03-27 13:27:46 +0400

    - Moving.

---
+++ include/mmdev.h
@@ -0,0 +1,37 @@
+/* Copyright (c) 2011-2014, Ailamazyan Program Systems Institute (Russian             
+ * Academy of Science). See COPYING in top-level directory. */
+
+/* vim: set syntax=c et sw=4 ts=4: */
+
+#ifndef MMDEV_H
+#define MMDEV_H
+
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "pautina-driver.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int mmdev_open (const char * pathname, int flags);
+extern int mmdev_close (int fd);
+extern int mmdev_ioctl (int fd, unsigned long request, pautina_driver_ioctl_arg_t arg);
+extern void * mmdev_mmap (void * addr, size_t length, int prot, int flags, int fd, off_t offset);
+extern int mmdev_munmap (void * addr, size_t length);
+
+extern void * mmdev_memcpy (void * dst, const void * src, size_t n);
+extern void mmdev_put64 (volatile void * ptr, uint64_t value);
+extern uint64_t mmdev_get64 (volatile void * ptr);
+extern void mmdev_fence();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MMDEV_H */
